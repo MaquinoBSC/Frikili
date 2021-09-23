0476bb4c607d8c267e8fafecc64136e9b2e215ec
Martin Hernandez <martin.hernandez@tagwizz.com.mx>
2021-09-23 17:13:54 -0500

    Agregar paginador usando knpPaginatorBundle

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Posts;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +14,21 @@ class DashboardController extends AbstractController
     /**
      * @Route("/dashboard", name="dashboard")
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
         $em= $this->getDoctrine()->getManager();
-        $posts= $em->getRepository(Posts::class)->findAll();
+        $query= $em->getRepository(Posts::class)->searchAllPosts();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            3 /*limit per page*/
+        );
+    
 
 
         return $this->render('dashboard/index.html.twig', [
-            'posts' => $posts,
+            'pagination' => $pagination,
         ]);
     }
 }
